f4b5e627e67eb29a8121404eef74c0a154ca008c
stephmukami <stephmukami@gmail.com>
2025-01-11 12:58:06 +0300

    feature: add conditional rendering

--- src/app/pages/product-list/product-card/product-card.component.ts
+++ src/app/pages/product-list/product-card/product-card.component.ts
@@ -21,6 +21,13 @@ import { PrimaryButtonComponent } from "../../../components/primary-button/prima
         <app-primary-button label="Add to Cart" class="mt-3"/>
       </div>
 
+      <span class="absolute top-2 right-3 text-sm font-bold">
+          @if (product().stock){
+            {{product().stock}} left
+          } 
+          @else { out of stock }
+          
+      </span>
 
     </div>
   `,
