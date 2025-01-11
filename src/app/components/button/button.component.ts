c8151aba96e506ace7daf9cecaed6cacf27f2dd0
stephmukami <stephmukami@gmail.com>
2025-01-11 22:32:53 +0300

    feature:add cart functionality

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  template: `
+     <button
+     class=" text-black w-full px-5 py-2 rounded-xl shadow-md hover:bg-slate-200"
+      (click)="btnClicked.emit()"
+    >
+      <span class="text-md">{{ label() }}</span>
+    </button>
+  `,
+  styles: ``
+})
+export class ButtonComponent {
+label = input('');
+btnClicked = output();
+}
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CartItemComponent } from "./cart-item/cart-item.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent],
   template: `
-    <p>
-      cart works!
-    </p>
+<div class="p-6 flex flex-col gap-4">
+      <h2 class="text-2xl">Shopping cart</h2>
+      @for( item of cartService.cart(); track item.id){
+          <app-cart-item [item] = "item"/>
+      }
+
+
+</div>
   `,
   styles: ``
 })
-export class CartComponent {
 
+export class CartComponent {
+cartService = inject(CartService);
 }
